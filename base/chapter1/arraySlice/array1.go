de99026f124587cbae023546df9d16ac71ea7880
jslearn <2608403303@qq.com>
2024-06-05 07:48:42 +0000

    检查某个值是否在数组中

--- base/chapter1/arraySlice/array1.go
+++ base/chapter1/arraySlice/array1.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, 世界")
+	// fmt.Println("Hello, 世界")
+	test()
+}
+
+// 检查字符串是否在切片zhong 
+func Exist(target string, array []string) bool {
+	for _, element := range array {
+		if element == target {
+			return true
+		}
+	}
+	return false
+}
+
+func test() {
+	nameList := []string{"zhong", "li", "li", "zhang"}
+	str1 := "zhong1"
+	str2 := "c++"
+	result := Exist(str1, nameList)
+	fmt.Println(result)
+	result = Exist(str2, nameList)
+	fmt.Println(result)
+	result = Exist("li", nameList)
+	fmt.Println(result)
 }
